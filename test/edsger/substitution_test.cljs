93ac7ef8f0c3aef3a0b687285c4ad732e230d32c
Laverne Schrock <schr1230@morris.umn.edu>
2017-08-27 13:31:16 -0500

    Add tests for substitution/highlight-elements

--- test/edsger/substitution_test.cljs
+++ test/edsger/substitution_test.cljs
@@ -2,5 +2,15 @@
   (:require [edsger.substitution :as subst]
             [clojure.test :as t :refer-macros [deftest is]]))
 
+(deftest highlight-elements-empty-list
+  (is (= '() (subst/highlight-elements '()))))
 
+(deftest highlight-elements-one-element
+  (is (= '((1)) (subst/highlight-elements '(1)))))
 
+(deftest highlight-elements-two-elements
+  (is (= '((1 2) (2 1)) (subst/highlight-elements '(1 2)))))
+
+(deftest highlight-elements-three-elements
+  (is (= '((1 2 3) (2 3 1) (3 1 2))
+         (subst/highlight-elements '(1 2 3)))))
