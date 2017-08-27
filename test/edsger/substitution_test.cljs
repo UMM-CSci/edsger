96aef45cde8334a25948b90e8564a24cd1955cae
Laverne Schrock <schr1230@morris.umn.edu>
2017-08-27 13:30:43 -0500

    Remove demo tests from substitution-test

--- test/edsger/substitution_test.cljs
+++ test/edsger/substitution_test.cljs
@@ -2,9 +2,5 @@
   (:require [edsger.substitution :as subst]
             [clojure.test :as t :refer-macros [deftest is]]))
 
-(deftest i-should-fail
-  (is (= 1 0)))
 
-(deftest i-should-succeed
-  (is (= 1 1)))
 
