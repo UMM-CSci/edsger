3791ae7585a09041f855b52f278679d880a60942
Laverne Schrock <schr1230@morris.umn.edu>
2017-09-15 00:06:45 -0500

    Replace falsey? with not
    'not' is the more standard way of doing this.

--- test/edsger/unification_test.cljs
+++ test/edsger/unification_test.cljs
@@ -2,8 +2,6 @@
   (:require [edsger.unification :as u]
             [cljs.test :as t :refer-macros [deftest is] :include-macros true]))
 
-(defn falsey? [x] (or (nil? x) (false? x)))
-
 (deftest a-test
   (is (true? (u/check-match '(:not (:equiv u (:or w y)))
                             '(:equiv (:not u) (:or w y))
@@ -11,7 +9,7 @@
                             '(:equiv (:not ?a) ?b)))))
 
 (deftest completely-inapplicable-rule
-  (is (falsey? (u/check-match '(:equiv (:and p q) p q)
+  (is (not (u/check-match '(:equiv (:and p q) p q)
                              '(:or p q)
                              '(:equiv (:and ?p ?q) ?p)
                              '(:equiv ?q (:or ?p ?q))))))
@@ -24,13 +22,13 @@
                             '(:equiv ?q ?q)))))
 
 (deftest proving-3_4-step-1-badv1
-  (is (falsey? (u/check-match '(true)
+  (is (not (u/check-match '(true)
                               '(:equiv p q)
                               '(true)
                               '(:equiv ?q ?q)))))
 
 (deftest proving-3_4-step-1-badv2
-  (is (falsey? (u/check-match '(false)
+  (is (not (u/check-match '(false)
                               '(:equiv q q)
                               '(true)
                               '(:equiv ?q ?q)))))
