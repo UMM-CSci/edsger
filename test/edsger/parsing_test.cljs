9484122c35628385991d4c64b1c0a1af23805e9e
Laverne Schrock <schr1230@morris.umn.edu>
2017-09-25 12:44:09 -0500

    Expand testing of lisp-style-cfg

--- test/edsger/parsing_test.cljs
+++ test/edsger/parsing_test.cljs
@@ -2,10 +2,33 @@
   (:require [edsger.parsing :as p]
             [clojure.test :as t :refer-macros [deftest is are] :include-macros true]))
 
-;; ==== Tests for lisp-style-cfg
+;; ==== Tests for `lisp-style-cfg`
 
 (deftest lisp-style-cfg_constant-values
   (are [input output] (= (p/lisp-style-cfg input) output)
     "(c)" [:S "(" [:A [:E "c"]] ")"]
     "(true)" [:S "(" [:A [:D "true"]] ")"]))
 
+(deftest lisp-style-cfg_single-operand
+  (are [input output] (= (p/lisp-style-cfg input) output)
+    "(not (a))" [:S "(" [:B "not"] " " [:S "(" [:A [:E "a"]] ")"] ")"]
+    "(not (false))" [:S "(" [:B "not"] " " [:S "(" [:A [:D "false"]] ")"] ")"]))
+
+(deftest lisp-style-cfg_two-operand
+  (are [input output] (= (p/lisp-style-cfg input) output)
+    "(or (false) (b))" [:S "("
+                        [:C "or"] " "
+                        [:S "(" [:A [:D "false"]] ")"] " "
+                        [:S "(" [:A [:E "b"]] ")"] ")"]
+    "(equiv (a) (b))" [:S "("
+                       [:C "equiv"] " "
+                       [:S "(" [:A [:E "a"]] ")"] " "
+                       [:S "(" [:A [:E "b"]] ")"] ")"]))
+
+(deftest list-style-cfg_unparsable-input
+  (are [input] (= (type (p/lisp-style-cfg input)) instaparse.gll/Failure)
+    ")"
+    "()"
+    "(true false)"
+    "and a b"
+    "(and (a) (b) (c))"))
