4636dfa43b5cf8dff0e6e8aaadbcd421f2c4aa87
nathan-beneke <benek020@morris.umn.edu>
2019-03-06 16:21:13 -0600

    Change (recursive validate ) in unification.cljs to return an array of booleans instead of a single boolean

--- src/edsger/unification.cljs
+++ src/edsger/unification.cljs
@@ -55,17 +55,17 @@
            (every? #(or (:match %) (:equal %)) result-list)))))))
 
 (defn recursive-validate
-    "validates each step in order, returning false if one fails and true if they all pass"
+    "validates each step in order, returning an array of boolean values corresponding to the rules, in order"
      [exps rules]
        (cond
              ;if one expression and 0 rules are left, all expressions and rules validated
-             (and (= 1 (count exps)) (empty? rules)) true
+             (and (= 1 (count exps)) (empty? rules)) '()
              ;if only one list is empty, something is wrong
-             (or (>= 1 (count exps)) (empty? rules)) false
+             (or (>= 1 (count exps)) (empty? rules)) (throw (Throwable. "Mismatched expression and rules lists' lengths"))
              (true? (check-match-recursive (nth exps 0) (nth exps 1) (nth rules 0) (nth rules 1)))
-                             (recursive-validate (rest exps) (rest (rest rules)))
+                             (cons true (recursive-validate (rest exps) (rest (rest rules))))
              ;if check-match-recursive didn't return true, end the computation
-             :else false))
+             :else (cons false (recursive-validate (rest exps) (rest (rest rules))))))
 
 ;; This file is currently only for playing around during development
 ;; but I think that we'll eventually have some useful functions here.
