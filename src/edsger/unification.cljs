4f19df2f1586f120541176ac55a6a2f0e46cb33d
nathan-beneke <benek020@morris.umn.edu>
2019-04-17 13:14:05 -0500

    Add check-implication to unification. Only matches top level functions

--- src/edsger/unification.cljs
+++ src/edsger/unification.cljs
@@ -67,6 +67,18 @@
              ;if check-match-recursive didn't return true, end the computation
              :else (cons false (recursive-validate (rest exps) (rest (rest rules))))))
 
+
+(defn check-implication
+  [exps rules]
+    (cond
+      ;if one expression and 0 rules are left, all expressions and rules validated
+      (and (= 1 (count exps)) (empty? rules)) '()
+      ;if only one list is empty, something is wrong
+      (or (>= 1 (count exps)) (empty? rules)) (throw (js/Error. "Mismatched expression and rules lists' lengths"))
+      (true? (check-match (nth exps 0) (nth exps 1) (nth rules 0) (nth rules 1)))
+                      (cons true (check-implication (rest exps) (rest (rest rules))))
+      ;if check-match didn't return true, cons a false on and keep going
+      :else (cons false (check-implication (rest exps) (rest (rest rules))))))
 ;; This file is currently only for playing around during development
 ;; but I think that we'll eventually have some useful functions here.
 
