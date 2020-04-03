8d9528fb82e52c8ef225167c142d584d741616ef
Trent Merkins <merki002@morris.umn.edu>
2020-04-03 14:37:37 -0500

    model checking program
    This refactors the program so that it takes the hiccup tree and sets it to expr. Then it calls on the programs in the rest of the document and checks that the rule passed to it is valid.

--- src/edsger/modelchecking.cljs
+++ src/edsger/modelchecking.cljs
@@ -2,35 +2,17 @@
 (ns powerset.core-test
   (:use (clojure set test)))
 
-(defn modelcheck
-   
-   (parse expression)
+(defn modelcheck [let [expr (parse [expression])]]
 
-    ;;find the variables and create a set
+  ;; evaluate expr if all true then it is correct
 
-    (let [op (first expr)]
-        (if ( = op variable)
-            (set (second expr))
-            (if (= op :not)
-                (variables (second expr))
-                (set/union (variables (second expr))
-                            (variables (third expr))
-                )
-            )
-        )
+    (if (some false (map #(eval-expr expr %) powerset[(variables)]))
+        (print "invalid rule")
     )
-   
+    (else( print "valid rule"))
+)
 
-
-    ;; Create a powerset of the set s containing all variables
-
-    (defn powerset [s]
-        ;; this information was on the github https://gist.github.com/dekellum/4171049
-        (apply union
-         #{s} ;the complete set of all s
-         (map (fn [i] (powerset (disj s i))) s)))
-
-
+(defn eval-expr
     (eval-expr expr true-vars
         (if(seq? expr)
             (eval-var expr true-vars)))
@@ -38,60 +20,30 @@
         (contains? true-vars v))
     (if(=:variable (first expr) 
         (eval-var(second expr) true-vars)))
+  )
 
-
-  ;; evaluate expr if all true then it is correct
-
-    (if ( = false (map #(eval-expr expr %) ps))
-        (print "invalid rule")
-    )
-    (else( print "valid rule"))
+(defn true-vars
+    
 )
 
-(defn check
-    ;; parse the expr
-    (parse expression)
-    ;; Search for all variables in the rule after parsed
+ ;; Create a powerset of the set s containing all variables
+(defn powerset [s]
+        ;; this information was on the github https://gist.github.com/dekellum/4171049
+        (apply union
+         #{s} ;the complete set of all s
+         (map (fn [i] (powerset (disj s i))) s)))
 
-   
-    
-    ;; Test using every combination of powerset
-        ;; create a loop i =0 to 2^(number of elements in a set)
-        
-        ;;all vars should initialize as false at the beginning of loop
-
-    ;; set vars in true-vars as true
-    (def true-vars map(i)
-        
+  ;;find the variables and create a set
+    (defn variables
+        (let [op (first expr)]
+            (if ( = op variable)
+                (set (second expr))
+                (if (= op :not)
+                    (variables (second expr))
+                    (union (variables (second expr))
+                            (variables (third expr))
+                     )
+                 )
+            )
+         )
     )
-    
-    ;; Use the functions and return boolean
-    (if(=:and)
-        (and var var))
-    (if(=:or)
-        (or var var))
-    (if(=:implies)
-        (implies var var))
-    (if(=:not)
-        (not var))
-    (if(=:equiv)
-        (equiv var var))
-
-   
-    ;; only boolean or var if the rule or expression as completely returned all values.
-    (if(var)
-         ;; If any of the final booleans return false then rule is invalid
-        (is false then exit))
-        ;;also should print invalid or say invalid and that rule does not evaluate correctly
-
-    ;; If all of the final booleans return true then rule is valid
-        ;; should have validate button or something say it is correct.
-    
-
-   
-
-        
-  
-
-)
-
