027ba5ebf7b3432dd55e3445c7fd2407e023349e
Trent Merkins <merki002@morris.umn.edu>
2020-04-24 15:02:46 -0500

    modelchecking fix
    fix eval expr as well as include a basic implies function

--- src/edsger/modelchecking.cljs
+++ src/edsger/modelchecking.cljs
@@ -1,26 +1,62 @@
-(ns edsger.modelchecking)
-(ns powerset.core-test
-  (:use (clojure set test)))
+(ns edsger.modelchecking
+  "Tools for model checking the validity of entered theorems"
+  (:use [clojure.set]))
 
-(defn modelcheck [let [expr (hiccup-tree)]]
-
+(defn modelcheck [expr]
+    
   ;; evaluate expr if all true then it is correct
 
-    (if (some false (map #(eval-expr expr %) (powerset[(variables)])))
-        (print "invalid rule")
-    )
-    (else( print "valid rule"))
+    (some false (map #(eval-expr [expr %]) (powerset[(variables[expr])])))
+    
 )
 
-(defn eval-expr
-    (eval-expr expr true-vars
-        (if(seq? expr)
-            (eval-var expr true-vars)))
-    (eval-var [v true-vars]
-        (contains? true-vars v))
-    (if(=:variable (first expr) 
-        (eval-var(second expr) true-vars)))
-  )
+; (eval-expr 'a #{'a 'b 'c}) => true
+; (eval-expr 'x #{'a 'b 'c}) => false
+;
+; (eval-expr [:and 'a 'b] #{'a 'b 'c}) => true
+; (eval-expr [:and 'a 'x] #{'a 'b 'c}) => false
+; 
+; (eval-var 'a #{'a 'b 'c}) => true
+; (eval-var 'x #{'a 'b 'c}) => false
+
+
+(defn eval-expr [expr true-vars]
+    (if (symbol? expr)
+        (eval-var expr true-vars)
+        (let [args (map #(eval-expr % true-vars) (rest expr))
+            (if (= :not (first expr)
+                (not (first args)
+                (if (= :and (first expr))
+                    (and (first args) (second args))
+                    (if (= :or (first expr))
+                        (or (first args)(second args))
+                        (if (= :implies (first expr))
+                            (implies (first args)(second args))
+                            (if (= :equiv (first expr))
+                                (equiv (first args)(second args)))))))))])))
+
+;(defn eval-expr [expr true-vars]
+;   (if (symbol? expr)
+;       (eval-var expr true-vars)
+;       let [args (map #(eval-expr % true-vars) (rest expr))
+;            first-arg (first args)
+;            second-arg (second args)]
+;           (condp = first expr)
+;               :not (not first-arg second-arg)
+;               :equiv (equiv first-arg second-arg)
+;               :implies (implies [first-arg] [second-arg])
+;               :and (and first-arg second-arg)
+;               :or (or first-arg second-arg)
+;               (str "not a valid operator")))))
+;               
+;   )
+;
+;
+;
+;
+
+;)
+
 
 
  ;; Create a powerset of the set s containing all variables
@@ -31,8 +67,8 @@
          (map (fn [i] (powerset (disj s i))) s)))
 
   ;;find the variables and create a set
-  
-(defn variables
+ 
+(defn variables [expr]
     (let [op (first expr)]
             (if ( = op variable)
                 (set (second expr))
@@ -44,4 +80,23 @@
                  )
             )
     )
+)
+
+(defn implies [arg0] [arg1]
+    (if (arg0)true
+        (if (arg1)false
+        false
+        )
+    )
+    else(
+        true)
+
+)
+
+(def op
+  {:not
+   :or
+   :and
+   :equiv
+   :implies}
 )
