2e5551814890ee42e0926d746ab23669e2e94da7
Trent Merkins <merki002@morris.umn.edu>
2020-04-29 13:48:51 -0500

    validation return
    This goes through the validation and returns a message corresponding to whether an item is valid. It will respond with a message on what to fix if invalid.

--- src/edsger/validation.cljs
+++ src/edsger/validation.cljs
@@ -3,32 +3,13 @@
     [edsger.unification :as uni])
 )
 
-(defn validation (exps rules step-types)
-    ;;rule invalid start
-
-    (if(mc/modelcheck rules)true
+(defn validation [exps rules step-types]
+    (if (mc/modelcheck rules)
         ;;rule is valid
-    )
-    (else
-        ;;rule is invalid fail and don't check matching
-        (try
-            (throw :type :custom-error
-            :message "Invalid rule.")
-        )
-    )
-
-    (if((uni/recursive-validate exps rules step-types)) true
-        ;;rule matches expression
-    )
-
-    (else
-        ;; valid rule, but it doesnt match.
-        (try
-            (throw :type :custom-error
-            :message "Rule is valid, but does not work with this expression.")
-        )
-    )
-
-
-
-)
+        (if (uni/recursive-validate exps rules step-types)
+            ;;rule matches expression
+            true
+            ;; valid rule, but it doesnt match.
+            :message "Rule is valid, but does not work with this expression."))
+    ;;rule is invalid fail and don't check matching
+    :message "Invalid rule.")
